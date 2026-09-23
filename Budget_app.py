91cc51315e2ada42b8d4754e5c58cb0473eed2fc
Lunacy <gettoworkritik12@gmail.com>
2026-09-23 14:26:12 +0530

    Add Category class and start create_spend_chart function

---
+++ Budget_app.py
@@ -0,0 +1,10 @@
+class Category:
+    def __init__(self,name,ledger):
+        self.name = name
+        self.ledger = []
+    def deposit(amount, description, self):
+        if not self.description
+
+
+def create_spend_chart(categories):
+    pass
